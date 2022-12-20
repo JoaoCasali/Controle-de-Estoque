7bf58108d7927d7a7d54b0bae6c83add1edbbad7
Guilherme L. Wamser <guilherme.wamser@proway.treina>
2022-12-19 21:58:34 -0300

    UP

--- entities/produto.py
+++ entities/produto.py
@@ -1,10 +1,12 @@
 from main import db
 class Produto(db.Model):
     
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    nome = db.Column(db.String(50), nullable=False)
-    categoria = db.Column(db.String(40), nullable=False)
-    marca = db.Column(db.String(20), nullable=False)
+    ID = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    DSC_PRO = db.Column(db.String(200), nullable=False)
+    MRC_DSC = db.Column(db.String(200), nullable=False)
+    UNI_MED = db.Column(db.String(50), nullable=False)
+    PRC_BRT = db.Column(db.Float, nullable=False)
+    #USU_ID = db.Column(db.Integer, db.ForeignKey('usuario.id'), nullable=False)
 
     def __repr__(self):
         return '<Name %r>' % self.name
--- main.py
+++ main.py
@@ -4,3 +4,31 @@ from config import *
 @app.route("/")
 def index():
     return "tela inicial"
+
+
+@app.route('/')
+def index():
+    lista = Produtos.query.order_by(Produtos.id)
+    return render_template("lista.html", titulo = 'produtos', produtos = lista)
+
+@app.route('/criar', methods=['POST',])
+def criar():
+    nome = request.form['nome']
+    marca = request.form['marca']
+    unidade = request.form['unidade']
+    preco = request.form['preco']
+
+    produto = Produtos.query.filter_by(nome=nome).first()
+
+    if produto:
+        flash('Produto Ja Existente!')
+        return redirect(url_for('index'))
+
+
+    novo_produto = Produtos(nome=nome, marca=marca, unidade=unidade, preco=preco)
+
+    db.session.add(novo_produto)
+
+    db.session.commit()
+
+    return redirect(url_for('index'))
