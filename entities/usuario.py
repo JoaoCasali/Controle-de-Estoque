374911b69ea9d9575275ab5130aabbf7c58df89f
Marcio Alberto De Oliveira Lourenco <marcio.lourenco@proway.treina>
2022-12-20 19:47:57 -0300

    atualizacao db

--- entities/usuario.py
+++ entities/usuario.py
@@ -1,5 +1,7 @@
-class Usuario:
-    def __init__(self, usuario, senha, nascimento):
-        self.usuario = usuario
-        self.senha = senha
-        self.nascimento = nascimento
+from config import db
+
+class Usuario(db.Model):
+    ID = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    NOM_PES = db.Column(db.Varchar(200), nullable=False)
+    SENHA = db.Column(db.Varchar(200), nullable=False)
+    DTA_NAS = db.Column(db.Varchar(200), nullable=False)
