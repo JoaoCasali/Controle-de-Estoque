598b0393eff41b4e2cd595da3bc72ead8316e864
Marcos Silveira <marcosilveira.lg@gmail.com>
2022-12-20 21:02:45 -0300

    alguma coisa

--- entities/produto.py
+++ entities/produto.py
@@ -1,12 +1,12 @@
-from main import db
-class Produto(db.Model):
+from config import db
+class Produtos(db.Model):
     
     ID = db.Column(db.Integer, primary_key=True, autoincrement=True)
     DSC_PRO = db.Column(db.String(200), nullable=False)
     MRC_DSC = db.Column(db.String(200), nullable=False)
     UNI_MED = db.Column(db.String(50), nullable=False)
     PRC_BRT = db.Column(db.Float, nullable=False)
-    #USU_ID = db.Column(db.Integer, db.ForeignKey('usuario.id'), nullable=False)
+    USU_ID = db.Column(db.Integer, db.ForeignKey('usuario.id'), nullable=False)
 
     def __repr__(self):
         return '<Name %r>' % self.name
---
+++ static/js/table.js
@@ -0,0 +1,5 @@
+
+function AbrirBotoes(id) {
+    var botao = "#botoes_" + id;
+    $(botao).toggleClass("esconder")
+}
